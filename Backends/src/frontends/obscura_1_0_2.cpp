474f891e7865e68e4196461ab0a3a85f27b7f627
Timon Emken <timon.emken@fysik.su.se>
2022-03-31 16:44:25 +0200

    Add obscura frontend header and source

---
+++ Backends/src/frontends/obscura_1_0_2.cpp
@@ -0,0 +1,14 @@
+//   GAMBIT: Global and Modular BSM Inference Tool
+//   *********************************************
+///  \file
+///
+///  Frontend source for the obscura backend.
+///  *********************************************
+///
+///  Authors (add name and date if you modify):
+///
+///  \author Timon Emken
+///          (timon.emken@fysik.su.se)
+///  \date 2022 Mar
+///
+///  *********************************************
