1498d446cc44c036095fa5684c0dacc58293f90d
Andrew Fowlie <andrew.j.fowlie@gmail.com>
2023-05-08 12:14:03 +0800

    fix mock

--- ScannerBit/src/scanners/python/mock.py
+++ ScannerBit/src/scanners/python/mock.py
@@ -12,8 +12,8 @@ from import_mocker import ImportMocker
 
 
 @contextmanager
-def mock_gambit(dim=1, mpi_rank=0, mpi_size=1, version="1.0.0", name="Mock", log=print, loglike=lambda x: -x[0]**2,
-                prior_transform=lambda x: x, parameter_names=None):
+def mock_gambit(dim=1, mpi_rank=0, mpi_size=1, version="1.0.0", name="Mock", log=print, loglike_hypercube=lambda x: -x[0]**2,
+                prior_transform=lambda x: x, inverse_prior_transform=lambda x: x, parameter_names=None, log_prior_density=lambda x: 1.):
     """
     Context in which gambit dependencies are mocked to allow for easy testing
     """
@@ -34,8 +34,10 @@ def mock_gambit(dim=1, mpi_rank=0, mpi_size=1, version="1.0.0", name="Mock", log
     mocked.Scanner.version = PropertyMock(return_value=version)
     mocked.Scanner.name = PropertyMock(return_value=name)
     mocked.Scanner.log = MagicMock(side_effect=log)
-    mocked.Scanner.loglike = MagicMock(side_effect=loglike)
+    mocked.Scanner.loglike_hypercube = MagicMock(side_effect=loglike_hypercube)
+    mocked.Scanner.log_prior_density = MagicMock(side_effect=log_prior_density)
     mocked.Scanner.prior_transform = MagicMock(side_effect=prior_transform)
+    mocked.Scanner.inverse_prior_transform = MagicMock(side_effect=inverse_prior_transform)
     mocked.Scanner.parameter_names = PropertyMock(return_value=parameter_names)
 
     yield mocked
