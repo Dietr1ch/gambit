032e28885942eb7c7067d4dd2ec2073adba460a1
Andrew Fowlie <andrew.j.fowlie@gmail.com>
2023-06-15 10:54:09 +0800

    add kombine sampler

---
+++ ScannerBit/src/scanners/python/plugins/gambit_kombine.py
@@ -0,0 +1,51 @@
+"""
+Kombine MCMC sampler
+====================
+"""
+
+import pickle
+import numpy as np
+
+import kombine
+
+from utils import Scanner, copydoc, version
+
+
+class Kombine(Scanner):
+    """
+    MCMC sampler with KDE proposals
+    """
+
+    name = "kombine"
+    __version__ = version("kombine")
+
+    def initial_state(self):
+        """
+        :returns: Choice of initial state for chain
+        """
+        return np.vstack([self.prior_transform(np.random.rand(self.dim))
+                         for i in range(self.nwalkers)])
+
+    @copydoc(kombine.Sampler)
+    def __init__(self, nwalkers=4, **kwargs):
+        self.nwalkers = nwalkers
+        self.sampler = kombine.Sampler(self.nwalkers, self.dim, self.log_target_density)
+
+    @copydoc(kombine.Sampler.run_mcmc)
+     def run(self, nsteps=5000, pkl_name="kombine.pkl", initial_state=None, **kwargs):
+        """
+        There are two additional arguments:
+
+        :param: pkl_name ('kombine.pkl')
+        :param: initial_state (None)
+
+        for passing the name of a pkl file to which to save results and the initial state.
+        """
+        if initial_state is None:
+            initial_state = self.initial_state()
+        self.sampler.burn_in(initial_state)
+        res = self.sampler.run_mcmc(nsteps, **kwargs)
+        with open(pkl_name, "wb") as f:
+            pickle.dump(res, f)
+
+__plugins__ = {Kombine.name: Kombine}
