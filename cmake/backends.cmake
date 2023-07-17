1778d660bea9e9c8ab19743d284d8a0691322bbb
Tomas Gonzalo <tomas.gonzalo@kit.edu>
2023-07-17 10:17:29 +0100

    Obscura builds in parallel

--- cmake/backends.cmake
+++ cmake/backends.cmake
@@ -1155,7 +1155,7 @@ if(NOT ditched_${name}_${ver})
     BUILD_IN_SOURCE 1
     PATCH_COMMAND patch -p1 < ${patch}
     CMAKE_ARGS -DCMAKE_CXX_COMPILER=${CMAKE_CXX_COMPILER} -DCMAKE_CXX_FLAGS=${BACKEND_CXX_FLAGS} -DCMAKE_C_COMPILER=${CMAKE_C_COMPILER} -DCMAKE_C_FLAGS=${BACKEND_C_FLAGS} -DBUILD_SHARED_LIBS=ON -DBUILD_TESTING=OFF -DCODE_COVERAGE=OFF -DCMAKE_BUILD_TYPE=Release
-    BUILD_COMMAND ${CMAKE_COMMAND} --build ${dir}
+    BUILD_COMMAND ${MAKE_PARALLEL} ${dir}
     INSTALL_COMMAND ""
   )
   add_extra_targets("backend" ${name} ${ver} ${libphysica_dir} ${dl} clean)
@@ -1192,8 +1192,8 @@ if(NOT ditched_${name}_${ver})
           COMMAND  ${CMAKE_COMMAND} -E make_directory "${dir}/generated/"
           COMMAND ${CMAKE_COMMAND} -E echo "" > "${dir}/generated/version.hpp"
     CMAKE_ARGS -DCMAKE_CXX_COMPILER=${CMAKE_CXX_COMPILER} -DCMAKE_CXX_FLAGS=${obscura_CXX_FLAGS} -DCMAKE_C_COMPILER=${CMAKE_C_COMPILER} -DCMAKE_C_FLAGS=${obscura_C_FLAGS} -DBUILD_SHARED_LIBS=ON -DBUILD_TESTING=OFF -DCODE_COVERAGE=OFF -DCMAKE_BUILD_TYPE=Release ${dir} -Dlibphysica_SOURCE_DIR=${libphysica_dir}
-    BUILD_COMMAND ${CMAKE_COMMAND} --build ${dir} --config Release
-    INSTALL_COMMAND ${CMAKE_COMMAND} --install ${dir}
+    BUILD_COMMAND ${MAKE_PARALLEL}
+    INSTALL_COMMAND ""#${MAKE_PARALLEL} install
   )
   add_extra_targets("backend" ${name} ${ver} ${dir} ${dl} clean)
   set_as_default_version("backend" ${name} ${ver})
