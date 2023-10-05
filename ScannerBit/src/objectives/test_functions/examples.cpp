6e5a11d6155f4bf6d64300a57e8b39f874531657
Gregory David Martinez <gregory.david.martinez@gmail.com>
2023-10-05 14:10:33 -0700

    removed debug cout.

--- ScannerBit/src/objectives/test_functions/examples.cpp
+++ ScannerBit/src/objectives/test_functions/examples.cpp
@@ -157,7 +157,7 @@ objective_plugin(EggBox, version(1, 0, 0))
         double params[2];
         params[0] = map[get_keys()[0]]*length.first;
         params[1] = map[get_keys()[1]]*length.second;
-        std::cout << "params: " << params[0] << ", " << params[1] << std::endl;
+        
         return 5.0*std::log(2.0 + std::cos(params[0]*M_PI_2)*std::cos(params[1]*M_PI_2));
     }
 }
