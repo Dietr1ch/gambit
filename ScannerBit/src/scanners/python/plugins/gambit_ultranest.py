90aac4b83b0f971a9a1995704ca1d630f931edef
Gregory David Martinez <gregory.david.martinez@gmail.com>
2023-10-06 10:15:36 -0700

    MPIed ultranest python plugin scanner.

--- ScannerBit/src/scanners/python/plugins/gambit_ultranest.py
+++ ScannerBit/src/scanners/python/plugins/gambit_ultranest.py
@@ -5,6 +5,7 @@ Ultranest scanners
 
 import pickle
 import ultranest
+from mpi4py import MPI
 
 import scanner_plugin as splug
 from utils import copydoc, version
@@ -17,11 +18,26 @@ class ReactiveUltranest(splug.scanner):
     name = "reactive_ultranest"
     __version__ = version(ultranest)
     
+    def ultra_transform(self, cube):
+        #print("transform = ", cube, self.mpi_rank)
+        
+        return self.transform_to_vec(cube)
+    
     def ultra_like(self, cube):
         lnew = self.loglike(cube)
         self.saves[tuple(cube)] = (self.mpi_rank, self.point_id)
-        
+        #print("like = ", cube, self.mpi_rank)
         return lnew
+    
+    def transfer(self):
+        comm = MPI.COMM_WORLD
+        
+        if self.mpi_rank == 0:
+            for i in range(1, self.mpi_size):
+                data = comm.recv(source=i, tag=1)
+                self.saves.update(data)
+        else:
+            comm.send(self.saves, dest=0, tag=1)
     
     @copydoc(ultranest.ReactiveNestedSampler)
     def __init__(self, log_dir="ultranest_log_dir", **kwargs):
@@ -30,12 +46,12 @@ class ReactiveUltranest(splug.scanner):
 
         :param: log_dir ('reactive_ultranest_run')
         """
-        super().__init__(use_mpi=False) # False for now.
+        super().__init__()
         self.saves = {}
         self.sampler = ultranest.ReactiveNestedSampler(
             self.parameter_names,
             self.ultra_like,
-            transform=self.transform_to_vec,
+            transform=self.ultra_transform,
             log_dir=log_dir,
             **self.init_args)
 
@@ -51,19 +67,22 @@ class ReactiveUltranest(splug.scanner):
         """
         
         self.sampler.run(**kwargs)
-        result = self.sampler.results
-        wts = result["weighted_samples"]["weights"]
-        upts = result["weighted_samples"]["upoints"]
+        self.transfer()
         
-        for wt, upt in zip(wts, upts):
-            if tuple(upt) in self.saves:
-                save = self.saves[tuple(upt)]
-                self.print(wt, "Posterior", save[0], save[1])
-            else:
-                print("warning: point has no correponding id.")
-                    
-        with open(pkl_name, "wb") as f:
-            pickle.dump(result, f)
+        if self.mpi_rank == 0:
+            result = self.sampler.results
+            wts = result["weighted_samples"]["weights"]
+            upts = result["weighted_samples"]["upoints"]
+            
+            for wt, upt in zip(wts, upts):
+                if tuple(upt) in self.saves:
+                    save = self.saves[tuple(upt)]
+                    self.print(wt, "Posterior", save[0], save[1])
+                else:
+                    print("warning: point has no correponding id.")
+                        
+            with open(pkl_name, "wb") as f:
+                pickle.dump(result, f)
     
     @copydoc(ultranest.ReactiveNestedSampler.run)
     def run(self):
