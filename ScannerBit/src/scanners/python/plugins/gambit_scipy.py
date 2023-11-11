950c3f194295ad0b92f16fb2f17fb321f60e9016
Anders Kvellestad <anders.kvellestad@fys.uio.no>
2023-11-11 19:33:18 +0100

    Fixed the scipy optimisers so they actually optimise the loglike function. Added some extra output and improved the parsing of run settings. Updated yaml files with corresponding settings.

--- ScannerBit/src/scanners/python/plugins/gambit_scipy.py
+++ ScannerBit/src/scanners/python/plugins/gambit_scipy.py
@@ -35,9 +35,25 @@ Dual annealing optimizer from scipy.
 
     @copydoc(scipy_optimize_dual_annealing)
     def run(self):
+
+        # If an initial x0 is given in self.run_args, 
+        # extract it and transform it to the unit hypercube
+        x0_unit = None
+        if 'x0' in self.run_args:
+            x0_phys = self.run_args.pop('x0')
+            x0_unit = self.inverse_transform(x0_phys)
+
         bounds = [(0., 1.)] * self.dim
-        res = scipy.optimize.dual_annealing(self.loglike_hypercube, bounds, **self.run_args)
+
+        def neg_loglike_hypercube(x):
+            return -self.loglike_hypercube(x)
+
+        res = scipy.optimize.dual_annealing(neg_loglike_hypercube, bounds, x0=x0_unit, **self.run_args)
+
+        print("\nOptimization summary from scipy_dual_annealing, with x-coordinates given in the unit hypercube in the order {0}:\n".format(self.parameter_names))
         print(res)
+        x_phys = self.transform_to_vec(res.x)
+        print("\nSolution x in physical coordinates: {0}\n".format(dict(zip(self.parameter_names, x_phys))))
         return 0
         
 
@@ -53,10 +69,24 @@ Basin-hopping optimizer from scipy.
 
     @copydoc(scipy_optimize_basinhopping)
     def run(self):
-        bounds = [(0., 1.)] * self.dim
-        res = scipy.optimize.basinhopping(
-            self.loglike_hypercube, bounds, **self.run_args)
+
+        x0_unit = None
+        if 'x0' in self.run_args:
+            x0_phys = self.run_args.pop('x0')
+            x0_unit = self.inverse_transform(x0_phys)
+        else:
+            # scipy.optimize.basinhopping requires an initial guess x0
+            raise Exception("Missing 'x0' dictionary in the scipy_basin_hopping run settings.")
+
+        def neg_loglike_hypercube(x):
+            return -self.loglike_hypercube(x)
+
+        res = scipy.optimize.basinhopping(neg_loglike_hypercube, x0_unit, **self.run_args)
+
+        print("\nOptimization summary from scipy_basin_hopping, with x-coordinates given in the unit hypercube in the order {0}:\n".format(self.parameter_names))
         print(res)
+        x_phys = self.transform_to_vec(res.x)
+        print("\nSolution x in physical coordinates: {0}\n".format(dict(zip(self.parameter_names, x_phys))))
         return 0
 
 
@@ -72,10 +102,25 @@ Differential evolution optimizer from scipy.
 
     @copydoc(scipy_optimize_differential_evolution)
     def run(self):
+
+        # If an initial x0 is given in self.run_args, 
+        # extract it and transform it to the unit hypercube
+        x0_unit = None
+        if 'x0' in self.run_args:
+            x0_phys = self.run_args.pop('x0')
+            x0_unit = self.inverse_transform(x0_phys)
+
         bounds = [(0., 1.)] * self.dim
-        res = scipy.optimize.differential_evolution(
-            self.loglike_hypercube, bounds, **self.run_args)
+
+        def neg_loglike_hypercube(x):
+            return -self.loglike_hypercube(x)
+
+        res = scipy.optimize.differential_evolution(neg_loglike_hypercube, bounds, x0=x0_unit, **self.run_args)
+
+        print("\nOptimization summary from scipy_differential_evolution, with x-coordinates given in the unit hypercube in the order {0}:\n".format(self.parameter_names))
         print(res)
+        x_phys = self.transform_to_vec(res.x)
+        print("\nSolution x in physical coordinates: {0}\n".format(dict(zip(self.parameter_names, x_phys))))
         return 0
         
 
@@ -91,10 +136,18 @@ The DIRECT optimizer from scipy.
 
     @copydoc(scipy_optimize_direct)
     def run(self):
-        print("DEBUG: kwargs: ", kwargs)
+
         bounds = [(0., 1.)] * self.dim
-        res = scipy.optimize.direct(self.loglike_hypercube, bounds, **self.run_args)
+
+        def neg_loglike_hypercube(x):
+            return -self.loglike_hypercube(x)
+
+        res = scipy.optimize.direct(neg_loglike_hypercube, bounds, **self.run_args)
+
+        print("\nOptimization summary from scipy_direct, with x-coordinates given in the unit hypercube in the order {0}:\n".format(self.parameter_names))
         print(res)
+        x_phys = self.transform_to_vec(res.x)
+        print("\nSolution x in physical coordinates: {0}\n".format(dict(zip(self.parameter_names, x_phys))))
         return 0
         
 
