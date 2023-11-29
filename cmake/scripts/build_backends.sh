c52d863e1efcd2669420cd17fc5dad00ccc08e93
Anders Kvellestad <anders.kvellestad@fys.uio.no>
2023-11-29 15:16:10 +0100

    Improved the build_backends.sh script. Can now take a list of backends to skip.

--- cmake/scripts/build_backends.sh
+++ cmake/scripts/build_backends.sh
@@ -1,34 +1,85 @@
-backends_list_file=$1
+# Usage: build_backends.sh -f <input file> -s <list of backends to skip> -j <number of cores>
+
+prefix="build_backends.sh:"
+make_j_option=1
+
+# Parse arguments
+while getopts "f:s:j:" opt; do
+  case $opt in
+    f) backends_list_file="${OPTARG}"
+    ;;
+    s) skip_backends="${OPTARG}"
+    ;;
+    j) make_j_option="${OPTARG}"
+    ;;
+    \?) printf "${prefix} Invalid option. Valid options: -f <input file>  -s <list of backends to skip>  -j <number of cores>\n" >&2
+    exit 1
+    ;;
+  esac
+
+  case $OPTARG in
+    -*) printf "${prefix} Option $opt needs a valid argument \n" >&2
+    exit 1
+    ;;
+  esac
+done
+
+if [ -z ${backends_list_file} ];
+then
+  printf "${prefix} Need an input file. Example: build_backends.sh -f <input file>\n" >&2
+  exit 1
+fi
 
 # Check that the input file exists
 if [ ! -f ${backends_list_file} ];
 then
-  printf "%s\n" "Cannot find the file ${backends_list_file}" >&2
+  printf "${prefix} Cannot find the file ${backends_list_file}\n" >&2
   exit 1
 fi
 
-# For each backend listed in default_backends.txt: 
-# - Try to build it
-# - If the build fails, add a line to failed_backends_file
+# For each backend listed in the input file:
+# - Check if the backend is in the $skip_backends list.
+# - If not, try to build it.
+# - If the build fails, add it to $failed_backends_list
 did_fail=0
 failed_backends_list=""
 while read backend || [[ -n $backend ]];
 do
-  make ${backend}
-  if [ $? -ne 0 ];
-  then 
-    did_fail=1
-    failed_backends_list="${failed_backends_list}, ${backend}"
+  do_skip_backend=0
+  for skip_backend_name in $(echo ${skip_backends}); 
+  do 
+    if [ ${skip_backend_name} = ${backend} ];
+    then
+      do_skip_backend=1
+    fi
+  done
+
+  if [ ${do_skip_backend} -eq 1 ];
+  then
+    printf "\n${prefix} Will skip backend ${backend}.\n"
+  else
+    printf "\n${prefix} Will now attempt to build backend ${backend}.\n"
+
+    # Now run the build command
+    make -j${make_j_option} ${backend}
+    if [ $? -ne 0 ];
+    then 
+      did_fail=1
+      failed_backends_list="${failed_backends_list}, ${backend}"
+    fi
   fi
 done < ${backends_list_file}
 
 failed_backends_list="${failed_backends_list:2}"
 
+# Report result
 if [ ${did_fail} -eq 1 ];
 then
-  printf "%s\n" "The following backends failed to build: ${failed_backends_list}" >&2
+  printf "\n${prefix} The following backends failed to build: ${failed_backends_list}\n" >&2
 else
-  printf "%s\n" "All backends built successfully!"
+  printf "\n${prefix} All backends built successfully!\n"
 fi
 
+# Return correct exit code
 exit ${did_fail}
+
